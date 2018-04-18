d97820cc64d8b0699ea5cc6854d09e266a416192
vetsinen <vetsinen@gmail.com>
2018-04-18 17:24:16 +0300

    initial commit

---
+++ city.php
@@ -0,0 +1,11 @@
+<?php
+
+$offices = [
+    'kiev, Ukraine'=>'zhilyanska, 5',
+    'Chernihiv, Ukraine' =>'lenina 24',
+    'Polotsk, Belorussia'=>'zubrovska, 7'
+];
+//foreach ($offices as $key=>$address){echo $key.PHP_EOL;}
+$ip = '185.230.124.199'; //$_SERVER['REMOTE_ADDR'];
+$details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
+print_r($details) ;
