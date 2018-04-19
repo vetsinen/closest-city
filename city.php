cdc106f3e1102b989b05e2e7191ada3d08607766
vetsinen <vetsinen@gmail.com>
2018-04-19 10:19:25 +0300

    basic info

--- city.php
+++ city.php
@@ -7,5 +7,6 @@ $offices = [
 ];
 //foreach ($offices as $key=>$address){echo $key.PHP_EOL;}
 $ip = '185.230.124.199'; //$_SERVER['REMOTE_ADDR'];
+echo "your ip is: $ip \n";
 $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
 print_r($details) ;
